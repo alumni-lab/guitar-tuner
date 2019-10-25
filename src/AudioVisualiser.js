75bc2d7f79e4013312e2a9cc9b60020a880fc915
Jerry Chuang <48072980+JerChuang@users.noreply.github.com>
2019-10-25 14:53:45 -0700

    Add code to find pitch/note/cents and console log

--- src/AudioVisualiser.js
+++ src/AudioVisualiser.js
@@ -1,34 +1,34 @@
 import React, { useEffect, createRef } from "react";
 
 const AudioVisualiser = ({ audioData }) => {
-  const canvasRef = createRef();
+  // const canvasRef = createRef();
 
-  const draw = () => {
-    const canvas = canvasRef.current;
-    const height = canvas.height;
-    const width = canvas.width;
-    const context = canvas.getContext("2d");
-    let x = 0;
-    const sliceWidth = (width * 1.0) / audioData.length;
-    context.lineWidth = 2;
-    context.strokeStyle = "#000000";
-    context.clearRect(0, 0, width, height);
-    context.beginPath();
-    context.moveTo(0, height / 2);
-    for (const item of audioData) {
-      const y = (item / 255.0) * height;
-      context.lineTo(x, y);
-      x += sliceWidth;
-    }
-    context.lineTo(x, height / 2);
-    context.stroke();
-  };
+  // const draw = () => {
+  //   const canvas = canvasRef.current;
+  //   const height = canvas.height;
+  //   const width = canvas.width;
+  //   const context = canvas.getContext("2d");
+  //   let x = 0;
+  //   const sliceWidth = (width * 1.0) / audioData.length;
+  //   context.lineWidth = 2;
+  //   context.strokeStyle = "#000000";
+  //   context.clearRect(0, 0, width, height);
+  //   context.beginPath();
+  //   context.moveTo(0, height / 2);
+  //   for (const item of audioData) {
+  //     const y = (item / 255.0) * height;
+  //     context.lineTo(x, y);
+  //     x += sliceWidth;
+  //   }
+  //   context.lineTo(x, height / 2);
+  //   context.stroke();
+  // };
 
-  useEffect(() => {
-    draw();
-  }, [canvasRef]);
+  // useEffect(() => {
+  //   draw();
+  // }, [canvasRef]);
 
-  return <canvas width="300" height="300" ref={canvasRef} />;
+  return <canvas />;
 };
 
 export default AudioVisualiser;
